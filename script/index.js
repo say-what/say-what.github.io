2a294c4bc8d325010c9f653e8c2cbcc01275cd37
Hai Joey Tran <htran@tgen.org>
2017-02-05 06:42:20 -0500

    make contact linkable on valid inp

--- script/index.js
+++ script/index.js
@@ -2,9 +2,11 @@
 $("#zip_input").keyup(function() {
     if (this.value.length == 5 && !isNaN(this.value)) {
         $("#zip_submit i").css('color','SteelBlue');
-        $("#zip_submit i").wrap( "<a href='representatives.html'></a>");
+        $("#zip_submit i").wrap( "<a href='contacting.html'></a>");
+        $(".nav-item:nth-of-type(2) a").removeClass('disabled');
     }else{
         $("#zip_submit a").removeAttr("href");
         $("#zip_submit i").css('color','#292b2c');
+        $(".nav-item:nth-of-type(2) a").addClass('disabled');
     }
 });
