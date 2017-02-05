a37abff51487924d7d17a92d6f2a9125762e463d
Hai Joey Tran <htran@tgen.org>
2017-02-05 08:04:24 -0500

    added contact param to href

--- script/index.js
+++ script/index.js
@@ -2,7 +2,7 @@
 $("#zip_input").keyup(function() {
     if (this.value.length == 5 && !isNaN(this.value)) {
         $("#zip_submit i").css('color','SteelBlue');
-        $("#zip_submit i").wrap( "<a href='contacting.html'></a>");
+        $("#zip_submit i").wrap( "<a href='contacting.html?" + this.value + "'></a>");
         $(".nav-item:nth-of-type(2) a").removeClass('disabled');
     }else{
         $("#zip_submit a").removeAttr("href");
