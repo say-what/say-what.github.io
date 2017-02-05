a2762a6cddfbebf525c915b17d56e504e27f7d0f
Hai Joey Tran <htran@tgen.org>
2017-02-05 06:28:34 -0500

    add href wrap

--- script/index.js
+++ script/index.js
@@ -2,7 +2,9 @@
 $("#zip_input").keyup(function() {
     if (this.value.length == 5 && !isNaN(this.value)) {
         $("#zip_submit i").css('color','SteelBlue');
+        $("#zip_submit i").wrap( "<a href='representatives.html'></a>");
     }else{
+        $("#zip_submit a").removeAttr("href");
         $("#zip_submit i").css('color','#292b2c');
     }
 });
